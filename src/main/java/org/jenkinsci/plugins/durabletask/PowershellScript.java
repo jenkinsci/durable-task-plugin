1ece0e69e7bf835a2f6858aa3d7efb345739e55d
gabloe <gabrielloewen@outlook.com>
2017-03-09 14:16:17 -0800

    More robust error handling and reporting

--- src/main/java/org/jenkinsci/plugins/durabletask/PowershellScript.java
+++ src/main/java/org/jenkinsci/plugins/durabletask/PowershellScript.java
@@ -75,21 +75,21 @@ public final class PowershellScript extends FileMonitoringTask {
         String cmd;
         if (capturingOutput) {
             // Using redirection in PowerShell produces extra newlines in output, so I am using [io.file]::WriteAllText to prevent corrupted output of exit code
-            cmd = String.format("$res = (& \"%s\" | Out-String) 2> \"%s\"; [io.file]::WriteAllText(\"%s\",$(if($LastExitCode -eq $null -or $LastExitCode -eq 0) {0} else {$LastExitCode})); [io.file]::WriteAllText(\"%s\",$res);", 
+            cmd = String.format("$res = (& \"%s\" | Out-String); 2> $null; [io.file]::WriteAllText(\"%s\",$LastExitCode); [io.file]::WriteAllText(\"%s\",$error); [io.file]::WriteAllText(\"%s\",$res);", 
                 quote(c.getPowershellMainFile(ws)),
-                quote(c.getLogFile(ws)),
                 quote(c.getResultFile(ws)),
+                quote(c.getLogFile(ws)),
                 quote(c.getOutputFile(ws)));
 
         } else {
-            cmd = String.format("$res = (& \"%s\" 2>&1 | Out-String); [io.file]::WriteAllText(\"%s\",$(if($LastExitCode -eq $null -or $LastExitCode -eq 0) {0} else {$LastExitCode})); [io.file]::WriteAllText(\"%s\",$res);",
+            cmd = String.format("$res = (& \"%s\" 2>&1 | Out-String); [io.file]::WriteAllText(\"%s\",$LastExitCode); [io.file]::WriteAllText(\"%s\",$res);",
                 quote(c.getPowershellMainFile(ws)),
                 quote(c.getResultFile(ws)),
                 quote(c.getLogFile(ws)));
         }
         
-        // Force the resulting script to exit with an exit code
-        String scriptWithExit = script + "\r\nexit $LastExitCode;";
+        // Force the resulting script to exit with an exit code.  This ensures that the result file will always have a non-null value
+        String scriptWithExit = "try {\r\n" + script + "\r\n} catch {\r\nexit 1;\r\n}\r\nif ($LastExitCode -ne $null -and $LastExitCode -ne 0) {\r\nexit $LastExitCode;\r\n} elseif ($error.Count -gt 0 -or !$?) {\r\nexit 1;\r\n} else {\r\nexit 0;\r\n}";
         
         // Write the script and execution wrapper to powershell files in the workspace
         c.getPowershellMainFile(ws).write(scriptWithExit, "UTF-8");
@@ -97,7 +97,7 @@ public final class PowershellScript extends FileMonitoringTask {
         
         // Try to load the PowerShell plugin to produce the command line arguments for running the script
         Plugin powershellPlugin = Jenkins.getInstance().getPlugin("powershell");
-        boolean loadError = false;
+        boolean notLoadedOrError = false;
         if (powershellPlugin != null && powershellPlugin.getWrapper().isEnabled()) {
             try {
                 ClassLoader cl = powershellPlugin.getWrapper().classLoader;
@@ -109,28 +109,30 @@ public final class PowershellScript extends FileMonitoringTask {
                 // Get the command line for running the script from the PowerShell plugin
                 args.addAll(Arrays.asList(cmdLine));
             } catch (ClassNotFoundException e) {
-                loadError = true;
+                notLoadedOrError = true;
                 listener.getLogger().println("Caught ClassNotFoundException: " + e.getMessage());
             } catch (InstantiationException e) {
-                loadError = true;
+                notLoadedOrError = true;
                 listener.getLogger().println("Caught InstantiationException: " + e.getMessage());
             } catch (NoSuchMethodException e) {
-                loadError = true;
+                notLoadedOrError = true;
                 listener.getLogger().println("Caught NoSuchMethodException: " + e.getMessage());
             } catch (IllegalAccessException e) {
-                loadError = true;
+                notLoadedOrError = true;
                 listener.getLogger().println("Caught IllegalAccessException: " + e.getMessage());
             } catch (InvocationTargetException e) {
-                loadError = true;
+                notLoadedOrError = true;
                 listener.getLogger().println("Caught InvocationTargetException: " + e.getMessage());
             } catch (Exception e) {
-                loadError = true;
+                notLoadedOrError = true;
                 listener.getLogger().println("Caught Exception: " + e.getMessage());
             }
+        } else {
+            notLoadedOrError = true;
         }
         
         // PowerShell plugin is not installed, not enabled, or cannot be loaded for some reason. Fall back using standard command line arguments
-        if (powershellPlugin == null || loadError == true) {
+        if (notLoadedOrError) {
             listener.getLogger().println("Warning: PowerShell plugin is not installed, not enabled, or cannot be loaded.  Executing script using the built in fallback.");
             if (launcher.isUnix()) {
                 // Open Powershell does not support ExecutionPolicy
