544944b7908b687bc58df44e895b882f949fbec2
gabloe <gabrielloewen@outlook.com>
2017-07-21 11:37:57 -0700

    Adding PowerShell helper function for proper BOM-free UTF8 file output

--- src/main/java/org/jenkinsci/plugins/durabletask/PowershellScript.java
+++ src/main/java/org/jenkinsci/plugins/durabletask/PowershellScript.java
@@ -69,20 +69,38 @@ public final class PowershellScript extends FileMonitoringTask {
         
         String cmd;
         if (capturingOutput) {
-            cmd = String.format("$(& \"%s\" | Out-File -FilePath \"%s\" -Encoding UTF8) 2>&1 3>&1 4>&1 5>&1 | Out-File -FilePath \"%s\" -Encoding UTF8; $LastExitCode | Out-File -FilePath \"%s\" -Encoding ASCII; $outputWithBom = Get-Content \"%s\"; [IO.File]::WriteAllLines(\"%s\", $outputWithBom);", 
+            cmd = String.format(". \"%s\"; $(& \"%s\" | Out-FileNoBom -FilePath \"%s\") 2>&1 3>&1 4>&1 5>&1 | Out-FileNoBom -FilePath \"%s\"; $LastExitCode | Out-File -FilePath \"%s\" -Encoding ASCII;", 
+                quote(c.getPowershellHelperFile(ws)),
                 quote(c.getPowershellMainFile(ws)),
                 quote(c.getOutputFile(ws)),
                 quote(c.getLogFile(ws)),
-                quote(c.getResultFile(ws)),
-                quote(c.getOutputFile(ws)),
-                quote(c.getOutputFile(ws)));
+                quote(c.getResultFile(ws)));
         } else {
-            cmd = String.format("& \"%s\" *>&1 | Out-File -FilePath \"%s\" -Encoding UTF8; $LastExitCode | Out-File -FilePath \"%s\" -Encoding ASCII;",
+            cmd = String.format(". \"%s\"; & \"%s\" *>&1 | Out-FileNoBom -FilePath \"%s\"; $LastExitCode | Out-File -FilePath \"%s\" -Encoding ASCII;",
+                quote(c.getPowershellHelperFile(ws)),
                 quote(c.getPowershellMainFile(ws)),
                 quote(c.getLogFile(ws)),
                 quote(c.getResultFile(ws)));
         }
        
+        // By default PowerShell adds a byte order mark (BOM) to the beginning of a file when using Out-File with a unicode encoding such as UTF8.
+        // This causes the Jenkins output to contain bogus characters because Java does not handle the BOM characters by default.
+        // This code mimics Out-File, but does not write a BOM.  Hopefully PowerShell will provide a non-BOM option for writing files in future releases.
+        String helperScript = "Function Out-FileNoBom {\n" +
+        "[CmdletBinding()]\n" +
+        "param(\n" +
+        "  [Parameter(Mandatory=$true, Position=0)] [string] $FilePath,\n" +
+        "  [Parameter(ValueFromPipeline=$true)] $InputObject\n" +
+        ")\n" +
+        "  $out = New-Object IO.StreamWriter $FilePath, $false\n" +
+        "  try {\n" +
+        "    $Input | Out-String -Stream | % { $out.WriteLine($_) }\n" +
+        "  } finally {\n" +
+        "    $out.Dispose()\n" +
+        "  }\n" +
+        "}";
+                              
+        writeWithBom(c.getPowershellHelperFile(ws), helperScript);
         writeWithBom(c.getPowershellScriptFile(ws),script);
         writeWithBom(c.getPowershellMainFile(ws),"try {\r\n& '" + quote(c.getPowershellScriptFile(ws)) + "'\r\n} catch {\r\nWrite-Error $_; exit 1;\r\n}\r\nfinally {\r\nif ($LastExitCode -ne $null) {\r\nexit $LastExitCode;\r\n} elseif ($error.Count -gt 0 -or !$?) {\r\nexit 1;\r\n} else {\r\nexit 0;\r\n}\r\n}");
         writeWithBom(c.getPowershellWrapperFile(ws),cmd);
@@ -106,6 +124,8 @@ public final class PowershellScript extends FileMonitoringTask {
         return f.getRemote().replace("$", "`$");
     }
     
+    // In order for PowerShell to properly read a script that contains unicode characters the script should have a BOM, but there is no built in support for
+    // writing UTF-8 with BOM in Java.  This code writes a UTF-8 BOM before writing the file contents.
     private static void writeWithBom(FilePath f, String contents) throws IOException, InterruptedException {
         OutputStream out = f.write();
         out.write(new byte[] { (byte)0xEF, (byte)0xBB, (byte)0xBF });
@@ -129,6 +149,14 @@ public final class PowershellScript extends FileMonitoringTask {
         
         public FilePath getPowershellWrapperFile(FilePath ws) throws IOException, InterruptedException {
             return controlDir(ws).child("powershellWrapper.ps1");
+        }
+        
+        public FilePath getTemporaryOutputFile(FilePath ws) throws IOException, InterruptedException {
+            return controlDir(ws).child("temporaryOutput.txt");
+        }
+        
+        public FilePath getPowershellHelperFile(FilePath ws) throws IOException, InterruptedException {
+            return controlDir(ws).child("powershellHelper.ps1");
         }
 
         private static final long serialVersionUID = 1L;
