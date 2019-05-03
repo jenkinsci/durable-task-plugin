34be7e07908d1601584109d4a8d82291a3c87b79
Chiou, Carroll <cchiou@cloudbees.com>
2019-05-03 00:28:27 -0700

    put heartbeat in a separate goroutin

--- src/main/resources/org/jenkinsci/plugins/durabletask/heartbeat-launcher.go
+++ src/main/resources/org/jenkinsci/plugins/durabletask/heartbeat-launcher.go
@@ -5,56 +5,108 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
+	"sync"
 	"syscall"
 	"time"
 )
 
-func launcher(pidChan chan int, scriptPath string, logPath string, resultPath string, outputPath string) {
-	scrptCmd := exec.Command("/bin/sh", "-c", scriptPath)
-	// scrptCmd.Stdout, err = os.OpenFile(logFile, os.O_WRONLY, 0644) //os.Stdout
-	logFile, err := os.Create(logPath)
+func checkHeartbeatErr(err error) {
+	checkErr("heartbeat", err)
+}
+func checkLauncherErr(err error) {
+	checkErr("launcher", err)
+}
+func checkErr(process string, err error) {
 	if err != nil {
-		os.Stderr.WriteString(err.Error())
+		errString := fmt.Sprintf("(%v) %v\n", process, err.Error())
+		os.Stderr.WriteString(errString)
 	}
+}
+func launcher(wg *sync.WaitGroup, pidChan chan int, scriptPath string, logPath string, resultPath string, outputPath string) {
+	defer wg.Done()
+	// Note: Writing the script exit code is handled in the process command because setsid
+	// applies only to the launched process and not the launcher. It is possible for the launcher
+	// to get killed before it is able to write the exit code.
+	// cmdString := scriptPath + "; echo $? > " + resultPath // + "; wait"
+	// cmdString := scriptPath + "; wait"
+	scrptCmd := exec.Command("/bin/sh", "-c", scriptPath)
+	logFile, err := os.Create(logPath)
+	checkLauncherErr(err)
+	defer logFile.Close()
+
 	if outputPath != "" {
 		// capturing output
 		outputFile, err := os.Create(outputPath)
-		if err != nil {
-			os.Stderr.WriteString(err.Error())
-		}
+		checkLauncherErr(err)
+		defer outputFile.Close()
 		scrptCmd.Stdout = outputFile
 	} else {
 		scrptCmd.Stdout = logFile
 	}
+	// capturing output
 	scrptCmd.Stderr = logFile
-	// if outputPath != "" {
-	// 	// capturing output
-	// 	scrptCmd.Stderr = logFile
-	// } else {
-	// 	scrptCmd.Stderr = os.Stdout
-	// }
+	// tee := io.MultiWriter(os.Stdout, logFile)
+	// scrptCmd.Stderr = tee
+	/*
+		if outputPath != "" {
+			// capturing output
+			teeWriter := io.MultiWriter(logFile, os.Stdout)
+			scrptCmd.Stderr = teeWriter //logFile
+		} else {
+			scrptCmd.Stderr = os.Stdout
+		}
+	*/
 	scrptCmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
+	scrptCmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 	for i := 0; i < len(scrptCmd.Args); i++ {
 		fmt.Printf("launcher args %v: %v\n", i, scrptCmd.Args[i])
 	}
 	scrptCmd.Start()
+	fmt.Printf("launcher: my pid (%v), launcher pid (%v)\n", os.Getpid(), scrptCmd.Process.Pid)
 	pidChan <- scrptCmd.Process.Pid
 	// If we do not call wait here, the forked process will be zombied until the main program exits
 	err = scrptCmd.Wait()
-	if err != nil {
-		errString := fmt.Sprintf("launcher exit error: %v\n", err.Error())
-		os.Stderr.WriteString(errString)
-	}
+	checkLauncherErr(err)
+	// /*
 	resultVal := scrptCmd.ProcessState.ExitCode()
-	// fmt.Printf("launcher script exit code: %v\n", resultVal)
+	fmt.Printf("launcher script exit code: %v\n", resultVal)
 	// result, err := os.OpenFile(resultFile, os.O_WRONLY, 0644)
-	result, err := os.Create(resultPath)
-	result.WriteString(strconv.Itoa(resultVal))
-	if err != nil {
-		os.Stderr.WriteString(err.Error())
-		return
-	}
+	fmt.Println("launcher writing result")
+	resultFile, err := os.Create(resultPath)
+	checkLauncherErr(err)
+	defer resultFile.Close()
+	resultFile.WriteString(strconv.Itoa(resultVal))
+	checkLauncherErr(err)
+	fmt.Println("about to close result file")
+	err = resultFile.Close()
+	checkLauncherErr(err)
+	// */
 	fmt.Println("launcher: done")
+}
+
+func heartbeat(wg *sync.WaitGroup, pidChan chan int, controlDir string, resultPath string, logPath string) {
+	defer wg.Done()
+	scriptPid := <-pidChan
+	for {
+		// send signal 0 because FindProcess will always return true for Unix
+		err := syscall.Kill(scriptPid, syscall.Signal(0))
+		fmt.Printf("heartbeat: process.Signal on pid %d returned: %v\n", scriptPid, err)
+		if err != nil {
+			break
+		}
+		_, err = os.Stat(controlDir)
+		if os.IsNotExist(err) {
+			break
+		}
+		_, err = os.Stat(resultPath)
+		if !os.IsNotExist(err) {
+			break
+		}
+		// heartbeat
+		err = os.Chtimes(logPath, time.Now(), time.Now())
+		checkHeartbeatErr(err)
+		time.Sleep(time.Second * 3)
+	}
 }
 
 func main() {
@@ -77,31 +129,36 @@ func main() {
 		outputPath = os.Args[5]
 	}
 
+	var wg sync.WaitGroup
 	pidChan := make(chan int)
-	go launcher(pidChan, scriptPath, logPath, resultPath, outputPath)
-	scriptPid := <-pidChan
-
-	for {
-		// send signal 0 because FindProcess will always return true for Unix
-		err := syscall.Kill(scriptPid, syscall.Signal(0))
-		fmt.Printf("process.Signal on pid %d returned: %v\n", scriptPid, err)
-		if err != nil {
-			break
+	wg.Add(2)
+	go launcher(&wg, pidChan, scriptPath, logPath, resultPath, outputPath)
+	go heartbeat(&wg, pidChan, controlDir, resultPath, logPath)
+	// scriptPid := <-pidChan
+	/*
+		for {
+			// send signal 0 because FindProcess will always return true for Unix
+			err := syscall.Kill(scriptPid, syscall.Signal(0))
+			fmt.Printf("process.Signal on pid %d returned: %v\n", scriptPid, err)
+			if err != nil {
+				break
+			}
+			_, err = os.Stat(controlDir)
+			if os.IsNotExist(err) {
+				break
+			}
+			_, err = os.Stat(resultPath)
+			if !os.IsNotExist(err) {
+				break
+			}
+			// heartbeat
+			err = os.Chtimes(logPath, time.Now(), time.Now())
+			if err != nil {
+				fmt.Printf("Chtimes error: %d", err)
+			}
+			time.Sleep(time.Second * 3)
 		}
-		_, err = os.Stat(controlDir)
-		if os.IsNotExist(err) {
-			break
-		}
-		_, err = os.Stat(resultPath)
-		if !os.IsNotExist(err) {
-			break
-		}
-		// heartbeat
-		err = os.Chtimes(logPath, time.Now(), time.Now())
-		if err != nil {
-			fmt.Printf("Chtimes error: %d", err)
-		}
-		time.Sleep(time.Second * 3)
-	}
+	*/
+	wg.Wait()
 	fmt.Println("main: done.")
 }
