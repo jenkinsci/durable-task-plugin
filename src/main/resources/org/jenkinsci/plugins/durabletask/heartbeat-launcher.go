cc1c80fefb01977cb44e9e8855e6d1d729df24b6
Chiou, Carroll <cchiou@cloudbees.com>
2019-05-10 10:32:56 -0700

    progress with double script launcher

--- src/main/resources/org/jenkinsci/plugins/durabletask/heartbeat-launcher.go
+++ src/main/resources/org/jenkinsci/plugins/durabletask/heartbeat-launcher.go
@@ -6,8 +6,8 @@ import (
 	"os/exec"
 	"os/signal"
 	"sync"
-	"syscall"
-	"time"
+
+	"golang.org/x/sys/unix"
 )
 
 func checkHeartbeatErr(err error) {
@@ -28,18 +28,25 @@ func checkErr(process string, err error) {
 // ensures that the main program will stay alive and record the exit code of the script.
 // Without it, there are possible race conditions in which the main program will terminate before
 // the script, and thus not record the exit code to the result file
-func signalCatcher(sigChan chan os.Signal) {
+func signalCatcher(sigChan chan os.Signal, pidChan chan int) {
+	scriptPid := <-pidChan
 	for sig := range sigChan {
 		fmt.Printf("(sig catcher) caught: %v\n", sig)
+		switch sig {
+		case unix.SIGTERM:
+			// TODO: caught sigterm, forward as sigint?
+			fmt.Printf("(sigcatcher) sending sigterm to %v\n", scriptPid)
+			unix.Kill(scriptPid, unix.SIGTERM)
+		}
 	}
 }
 
-func launcher(wg *sync.WaitGroup, doneChan chan bool,
-	scriptPath string, logPath string, resultPath string, outputPath string) {
+func launcher(wg *sync.WaitGroup, pidChan chan int,
+	scriptString string, logPath string, resultPath string, outputPath string) {
 
 	defer wg.Done()
 	recordExit := fmt.Sprintf("; echo $? > %v.tmp; mv %v.tmp %v; wait", resultPath, resultPath, resultPath)
-	scriptWithExit := scriptPath + recordExit
+	scriptWithExit := scriptString + recordExit
 	scriptCmd := exec.Command("/bin/sh", "-c", scriptWithExit)
 	logFile, err := os.Create(logPath)
 	checkLauncherErr(err)
@@ -62,13 +69,12 @@ func launcher(wg *sync.WaitGroup, doneChan chan bool,
 		scriptCmd.Stderr = scriptCmd.Stdout
 	}
 	// Prevents script from being terminated if program gets terminated
-	scriptCmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
-	// Allows child processes of the script to be killed if kill signal sent to script's process group id
-	scriptCmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	scriptCmd.SysProcAttr = &unix.SysProcAttr{Setsid: true}
 	for i := 0; i < len(scriptCmd.Args); i++ {
 		fmt.Printf("(launcher) args %v: %v\n", i, scriptCmd.Args[i])
 	}
 	scriptCmd.Start()
+	pidChan <- scriptCmd.Process.Pid
 	fmt.Printf("(launcher) my pid (%v), launcher pid (%v)\n", os.Getpid(), scriptCmd.Process.Pid)
 	// Note: If we do not call wait, the forked process will be zombied until the main program exits
 	// ignoring SIGCHLD is not as portable as calling Wait()
@@ -76,13 +82,14 @@ func launcher(wg *sync.WaitGroup, doneChan chan bool,
 	checkLauncherErr(err)
 	resultVal := scriptCmd.ProcessState.ExitCode()
 	fmt.Printf("(launcher) script exit code: %v\n", resultVal)
-	doneChan <- true
 }
 
-func heartbeat(wg *sync.WaitGroup, doneChan chan bool,
+func heartbeat(wg *sync.WaitGroup, pidChan chan int,
 	controlDir string, resultPath string, logPath string) {
 
 	defer wg.Done()
+	const HBSCRIPT string = "heartbeat.sh"
+	scriptPid := <-pidChan
 	_, err := os.Stat(controlDir)
 	if os.IsNotExist(err) {
 		fmt.Fprintf(os.Stderr, "%v\n", err.Error())
@@ -93,19 +100,23 @@ func heartbeat(wg *sync.WaitGroup, doneChan chan bool,
 		fmt.Printf("Result file already exists, stopping heartbeat.\n%v\n", resultPath)
 		return
 	}
-	for {
-		select {
-		case <-doneChan:
-			fmt.Println("(heartbeat) received script finished, exiting")
-			return
-		default:
-			// heartbeat
-			fmt.Println("(heartbeat) touch log")
-			err = os.Chtimes(logPath, time.Now(), time.Now())
-			checkHeartbeatErr(err)
-			time.Sleep(time.Second * 3)
-		}
-	}
+	// heartbeat script
+	heartbeat := fmt.Sprintf("while true ; do kill -0 %v; if [ $? -ne 0 ]; then echo '(heartbeat) process (%v) not found'; break; fi; touch %v; echo touched log; sleep 3; done",
+		scriptPid, scriptPid, logPath)
+	heartbeatScript, err := os.Create(HBSCRIPT)
+	checkHeartbeatErr(err)
+	heartbeatScript.WriteString(heartbeat)
+	heartbeatScript.Close()
+
+	// heartbeatCmd := exec.Command("/bin/sh", heartbeat)
+	heartbeatCmd := exec.Command("/bin/sh", HBSCRIPT)
+	heartbeatCmd.Stdout = os.Stdout
+	heartbeatCmd.Stderr = os.Stderr
+	heartbeatCmd.SysProcAttr = &unix.SysProcAttr{Setsid: true}
+	heartbeatCmd.Start()
+	fmt.Printf("(heartbeat) my pid (%v), heartbeat pid (%v)\n", os.Getpid(), heartbeatCmd.Process.Pid)
+	err = heartbeatCmd.Wait()
+	checkHeartbeatErr(err)
 }
 
 func main() {
@@ -116,30 +127,28 @@ func main() {
 	argLen := len(os.Args)
 	if argLen < 5 || argLen > 6 {
 		// invalid number of args
-		os.Stderr.WriteString("Input error: expected number of args is 5 or 6 (controlDir, resultPath, logPath, scriptPath, outputPath[opt])")
+		os.Stderr.WriteString("Input error: expected number of args is 5 or 6 (controlDir, resultPath, logPath, scriptString, outputPath[opt])")
 		return
 	}
 	controlDir := os.Args[1]
 	resultPath := os.Args[2]
 	logPath := os.Args[3]
-	scriptPath := os.Args[4]
+	scriptString := os.Args[4]
 	outputPath := ""
 	if argLen == 6 {
 		outputPath = os.Args[5]
 	}
 
+	pidChan := make(chan int)
 	sigChan := make(chan os.Signal, 1)
-	// Note: If signal.Ignore is used, this will be inherited by the script and it will be unable
-	// to terminate in the STOP unit test of the BourneShellScriptTest suite. This is beause (under mac)
-	// the script is terminated with SIGTERM instead of SIGKILL
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
-	go signalCatcher(sigChan)
+	signal.Notify(sigChan, unix.SIGTERM)
+	signal.Ignore(unix.SIGINT, unix.SIGHUP, unix.SIGQUIT)
+	go signalCatcher(sigChan, pidChan)
 
-	doneChan := make(chan bool)
 	var wg sync.WaitGroup
 	wg.Add(2)
-	go launcher(&wg, doneChan, scriptPath, logPath, resultPath, outputPath)
-	go heartbeat(&wg, doneChan, controlDir, resultPath, logPath)
+	go launcher(&wg, pidChan, scriptString, logPath, resultPath, outputPath)
+	go heartbeat(&wg, pidChan, controlDir, resultPath, logPath)
 	wg.Wait()
 	signal.Stop(sigChan)
 	close(sigChan)
