77058b3184e4a2ca25017664bab0ee9b0ec4c081
Chiou, Carroll <cchiou@cloudbees.com>
2019-05-24 14:19:06 -0700

    removed all output to stdout/stderr No change in behavior for terminal closure

--- src/main/go/org/jenkinsci/plugins/durabletask/heartbeat-launcher.go
+++ src/main/go/org/jenkinsci/plugins/durabletask/heartbeat-launcher.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -18,20 +17,23 @@ import (
 const trapSig = "trap ':' INT TERM EXIT"
 const base = "main"
 const hb = "heartbeat"
+const launch = "launcher"
 
-func checkHeartbeatErr(output io.Writer, err error) {
-	checkErr("heartbeat", err)
-}
+var debugLogger *log.Logger
 
-func checkLauncherErr(err error) {
-	checkErr("launcher", err)
-}
+// func checkHeartbeatErr(output io.Writer, err error) {
+// 	checkErr("heartbeat", err)
+// }
 
-func checkErr(process string, err error) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "(%v) check err: %v\n", strings.ToUpper(process), err.Error())
-	}
-}
+// func checkLauncherErr(err error) {
+// 	checkErr("launcher", err)
+// }
+
+// func checkErr(process string, err error) {
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "(%v) check err: %v\n", strings.ToUpper(process), err.Error())
+// 	}
+// }
 
 func logCheckErr(logger *log.Logger, err error) {
 	if err != nil {
@@ -40,9 +42,9 @@ func logCheckErr(logger *log.Logger, err error) {
 }
 
 // Catch a termination signal to allow for a graceful exit (i.e. no zombies)
-func signalCatcher(sigChan chan os.Signal) {
+func signalCatcher(sigChan chan os.Signal, logger *log.Logger) {
 	for sig := range sigChan {
-		fmt.Printf("(sig catcher) caught: %v\n", sig)
+		logger.Printf("(sig catcher) caught: %v\n", sig)
 	}
 }
 
@@ -55,13 +57,13 @@ func launcher(wg *sync.WaitGroup, exitChan chan bool,
 	scriptWithExit := trapSig + "; " + scriptString + "; " + recordExit
 	scriptCmd := exec.Command("/bin/sh", "-c", scriptWithExit)
 	logFile, err := os.Create(logPath)
-	checkLauncherErr(err)
+	logCheckErr(debugLogger, err)
 	defer logFile.Close()
 
 	if outputPath != "" {
 		// capturing output
 		outputFile, err := os.Create(outputPath)
-		checkLauncherErr(err)
+		logCheckErr(debugLogger, err)
 		defer outputFile.Close()
 		scriptCmd.Stdout = outputFile
 	} else {
@@ -77,39 +79,44 @@ func launcher(wg *sync.WaitGroup, exitChan chan bool,
 	// Prevents script from being terminated if program gets terminated
 	scriptCmd.SysProcAttr = &unix.SysProcAttr{Setsid: true}
 	for i := 0; i < len(scriptCmd.Args); i++ {
-		fmt.Printf("(launcher) args %v: %v\n", i, scriptCmd.Args[i])
+		// debugLogger.Printf("(launcher) args %v: %v\n", i, scriptCmd.Args[i])
+		fmt.Fprintf(logFile, "(launcher) args %v: %v\n", i, scriptCmd.Args[i])
 	}
 	scriptCmd.Start()
 	pid := scriptCmd.Process.Pid
 	// pidChan <- pid
-	fmt.Printf("(launcher) launched %v\n", pid)
+	// debugLogger.Printf("(launcher) launched %v\n", pid)
+	fmt.Fprintf(logFile, "(launcher) launched %v\n", pid)
 	err = scriptCmd.Wait()
-	checkLauncherErr(err)
+	logCheckErr(debugLogger, err)
 	resultVal := scriptCmd.ProcessState.ExitCode()
-	fmt.Printf("(launcher) script exit code: %v\n", resultVal)
+	// debugLogger.Printf("(launcher) script exit code: %v\n", resultVal)
+	fmt.Fprintf(logFile, "(launcher) script exit code: %v\n", resultVal)
 	exitChan <- true
+	// debugLogger.Printf("(launcher) signaled script exit\n")
+	fmt.Fprintf(logFile, "(launcher) signaled script exit\n")
 }
 
 func heartbeat(wg *sync.WaitGroup, exitChan chan bool,
 	controlDir string, resultPath string, logPath string) {
 
 	defer wg.Done()
-	debugFile, debugErr := os.Create(controlDir + hb + ".log")
-	checkErr(hb, debugErr)
-	defer debugFile.Close()
+	hbDebug, hbErr := os.Create(controlDir + hb + ".log")
+	logCheckErr(debugLogger, hbErr)
+	defer hbDebug.Close()
 	debugPrefix := strings.ToUpper(hb) + " "
-	logger := log.New(debugFile, debugPrefix, log.Ltime|log.Lshortfile)
+	hbLogger := log.New(hbDebug, debugPrefix, log.Ltime|log.Lshortfile)
 
 	// const HBSCRIPT string = "heartbeat.sh"
 	// fmt.Printf("(heartbeat) checking if %v is alive\n", launchedPid)
 	_, err := os.Stat(controlDir)
 	if os.IsNotExist(err) {
-		logger.Printf("%v\n", err.Error())
+		hbLogger.Printf("%v\n", err.Error())
 		return
 	}
 	_, err = os.Stat(resultPath)
 	if !os.IsNotExist(err) {
-		logger.Printf("Result file already exists, stopping heartbeat.\n%v\n", resultPath)
+		hbLogger.Printf("Result file already exists, stopping heartbeat.\n%v\n", resultPath)
 		return
 	}
 	// create the heartbeat script
@@ -144,17 +151,17 @@ func heartbeat(wg *sync.WaitGroup, exitChan chan bool,
 	for {
 		select {
 		case <-exitChan:
-			logger.Println("received script finished, exiting")
+			hbLogger.Println("received script finished, exiting")
 			return
 		default:
 			// heartbeat
-			logger.Println("touch log")
+			hbLogger.Println("touch log")
 			err = os.Chtimes(logPath, time.Now(), time.Now())
-			logCheckErr(logger, err)
+			logCheckErr(hbLogger, err)
 			time.Sleep(time.Second * 3)
 		}
 	}
-	// logger.Println("exit")
+	// hbLogger.Println("exit")
 }
 
 func main() {
@@ -174,10 +181,13 @@ func main() {
 	}
 
 	debugFile, debugErr := os.Create(controlDir + base + ".log")
-	checkErr(base, debugErr)
+	if debugErr != nil {
+		// os.Stderr.WriteString("unable to create debug log file\n")
+		return
+	}
 	defer debugFile.Close()
 	debugPrefix := strings.ToUpper(base) + " "
-	debugLogger := log.New(debugFile, debugPrefix, log.Ltime|log.Lshortfile)
+	debugLogger = log.New(debugFile, debugPrefix, log.Ltime|log.Lshortfile)
 
 	debugLogger.Printf("number of args: %v\n", os.Args)
 	for i := 0; i < len(os.Args); i++ {
@@ -189,7 +199,7 @@ func main() {
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, unix.SIGINT, unix.SIGTERM)
-	go signalCatcher(sigChan)
+	go signalCatcher(sigChan, debugLogger)
 
 	exitChan := make(chan bool)
 	var wg sync.WaitGroup
