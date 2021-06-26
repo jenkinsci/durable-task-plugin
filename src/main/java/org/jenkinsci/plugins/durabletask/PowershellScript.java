8a57ab74e82e3ef7ee6d740138f43c22b2e775a7
Carroll Chiou <cchiou@cloudbees.com>
2021-06-25 22:13:32 -0600

    fix whitespace

--- src/main/java/org/jenkinsci/plugins/durabletask/PowershellScript.java
+++ src/main/java/org/jenkinsci/plugins/durabletask/PowershellScript.java
@@ -228,17 +228,17 @@ public final class PowershellScript extends FileMonitoringTask {
         String cmd;
         if (capturingOutput) {
             cmd = String.format(". '%s'; Execute-AndWriteOutput -MainScript '%s' -OutputFile '%s' -LogFile '%s' -ResultFile '%s' -CaptureOutput;",
-                    quote(c.getPowerShellHelperFile(ws)),
-                    quote(c.getPowerShellScriptFile(ws)),
-                    quote(c.getOutputFile(ws)),
-                    quote(c.getLogFile(ws)),
-                    quote(c.getResultFile(ws)));
+                quote(c.getPowerShellHelperFile(ws)),
+                quote(c.getPowerShellScriptFile(ws)),
+                quote(c.getOutputFile(ws)),
+                quote(c.getLogFile(ws)),
+                quote(c.getResultFile(ws)));
         } else {
             cmd = String.format(". '%s'; Execute-AndWriteOutput -MainScript '%s' -LogFile '%s' -ResultFile '%s';",
-                    quote(c.getPowerShellHelperFile(ws)),
-                    quote(c.getPowerShellWrapperFile(ws)),
-                    quote(c.getLogFile(ws)),
-                    quote(c.getResultFile(ws)));
+                quote(c.getPowerShellHelperFile(ws)),
+                quote(c.getPowerShellWrapperFile(ws)),
+                quote(c.getLogFile(ws)),
+                quote(c.getResultFile(ws)));
         }
 
         args.add(powershellBinary);
