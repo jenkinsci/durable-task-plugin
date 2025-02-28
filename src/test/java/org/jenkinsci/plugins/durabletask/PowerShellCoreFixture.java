a54f008da8e8859f56a59046bb7d9cccea2bb790
Basil Crow <me@basilcrow.com>
2025-02-28 08:01:21 -0800

    Require 2.479.1 or newer (#245)

--- src/test/java/org/jenkinsci/plugins/durabletask/PowerShellCoreFixture.java
+++ src/test/java/org/jenkinsci/plugins/durabletask/PowerShellCoreFixture.java
@@ -5,5 +5,5 @@ import org.jenkinsci.test.acceptance.docker.DockerFixture;
 
 @DockerFixture(id = "pwsh", ports = 22)
 public class PowerShellCoreFixture extends DockerContainer {
-    public static final String PWSH_JAVA_LOCATION = "/usr/lib/jvm/java-11-openjdk-amd64/bin/java";
+    public static final String PWSH_JAVA_LOCATION = "/usr/lib/jvm/java-17-openjdk-amd64/bin/java";
 }
